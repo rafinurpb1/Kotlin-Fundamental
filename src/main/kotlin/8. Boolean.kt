ad59872eb650a259a37abeed0485c7dfb3909868
ZalEmperon <flowgreen173@gmail.com>
2020-08-12 13:08:13 +0700

    Kotlinn

--- src/main/kotlin/8. Boolean.kt
+++ src/main/kotlin/8. Boolean.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val hasil = 49
+    val hasil = 75
     val nilai: String
     val nilaiE = hasil >= 50
     nilai = if (hasil <= 100 && hasil >= 90) {
@@ -8,12 +8,10 @@ fun main() {
         "B"
     } else if (hasil <= 79 && hasil >= 70) {
         "C"
-    } else if (hasil <= 70 && hasil >= 60) {
+    } else if (hasil <= 69 && hasil >= 60) {
         "D"
-    } else if (!nilaiE){
-        "E"
     } else {
-        ""
+        "E"
     }
     print(nilai)
 }
